f5229799b2e7f7771f9bd2a8ab7b60fda3d22330
Aleksei <aleksei.karpukovich@student.ehu.lt>
2024-07-05 19:58:08 +0300

    fix: vite config

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,6 @@
 import { defineConfig } from 'vite'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/co-shop/',
-  build: {
-    outDir: 'dist',
-    rollupOptions: {
-      input: {
-        main: 'index.html'
-      }
-    }
-  }
+   base: '/vite-deploy-demo/'
 })
